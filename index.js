e38d15b71896cfc9a77c534ee23d4c8c1424f00a
Surya Pratap Singh <suryaprataps424@gmail.com>
2021-09-11 11:40:43 +0530

    added api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
-app.post("/update", (req, res) => {
-    res.send('hii')
+const alldata = require('./database')
+app.get("/data", (req, res) => {
+    res.send(alldata)
 })
 app.listen(process.env.PORT || 80, () => {
     console.log("server running at http://localhost:80");
